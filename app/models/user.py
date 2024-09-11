4630b0516649570f307c5d228c21a4c2bbdcc429
fabiiann1 <fabianmarinn1@gmail.com>
2024-09-11 10:07:37 -0500

    A

--- app/models/user.py
+++ app/models/user.py
@@ -3,14 +3,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from app import db
 
 class Users(db.Model):
-
     __tablename__ = 'user' 
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
-    eco_score = db.Column(db.Integer, default=0)
+    eco_score = db.Column(db.Integer, default=0)        
     carbon_footprint = db.Column(db.Float, default=0.0)
 
     def set_password(self, password):
