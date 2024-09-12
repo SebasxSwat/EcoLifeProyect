8e525247cd4a342f71ba7323c45800f46a96cff6
SebasxSwat <sebastianescalante2006@gmail.com>
2024-09-12 10:24:37 -0500

    S

--- app/routes/authRoute.py
+++ app/routes/authRoute.py
@@ -1,19 +1,39 @@
 from flask import Blueprint, request, jsonify
-from app.models.user import User
 from app import db
-from 
+from app.models import User  
 
-bp = Blueprint('auth', __name__)
+bp = Blueprint('auth', __name__, url_prefix='/auth')
 
-@bp.route('/', methods=['POST'])
-def create_user():
+@bp.route('/signup', methods=['POST'])
+def signup():
     data = request.get_json()
-    user = User(**data)
-    try:
-        db.session.add(user)
-        db.session.commit()
-        return jsonify(user.to_json()), 201
-    except IntegrityError as e:
-        db.session.rollback()
-        return jsonify({"error": str(e.orig)}), 400
+    username = data.get('username')
+    email = data.get('email')
+    password = data.get('password')  
+    eco_score = data.get('eco_score', 0)
+    carbon_footprint = data.get('carbon_footprint', 0.0)
 
+    if not username or not email or not password:
+        return jsonify({'error': 'Missing required fields'}), 400
+
+    if User.query.filter_by(username=username).first() or User.query.filter_by(email=email).first():
+        return jsonify({'error': 'User already exists'}), 400
+
+    new_user = User(
+        username=username,
+        email=email,
+        password=password, 
+        eco_score=eco_score,
+        carbon_footprint=carbon_footprint
+    )
+
+    db.session.add(new_user)
+    db.session.commit()
+
+    return jsonify({
+        'id': new_user.id,
+        'username': new_user.username,
+        'email': new_user.email,
+        'eco_score': new_user.eco_score,
+        'carbon_footprint': new_user.carbon_footprint
+    }), 201
