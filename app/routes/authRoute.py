3a74f5f4e5bd71a711d79aada244069d56c05d38
SebasxSwat <sebastianescalante2006@gmail.com>
2024-09-12 11:56:11 -0500

    S

--- app/routes/authRoute.py
+++ app/routes/authRoute.py
@@ -1,12 +1,16 @@
 from flask import Blueprint, request, jsonify
 from app import db
-from app.models import User  
+from app.models import User
+from werkzeug.security import check_password_hash
+import jwt
+import datetime
 
 bp = Blueprint('auth', __name__, url_prefix='/auth')
 
+# Ruta para el registro de usuarios
 @bp.route('/register', methods=['POST'])
 def register():
-    data = request.get_json()  
+    data = request.get_json()
 
     if not all(key in data for key in ('username', 'email', 'password')):
         return jsonify({"message": "Faltan campos requeridos"}), 400
@@ -23,3 +27,28 @@ def register():
     db.session.commit()
 
     return jsonify({"message": "Registro exitoso"}), 201
+
+@bp.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+
+    if not all(key in data for key in ('username', 'password')):
+        return jsonify({"message": "Faltan campos requeridos"}), 400
+
+    username = data['username']
+    password = data['password']
+    
+    user = User.query.filter_by(username=username).first()
+
+    if not user or user.password != password:
+        return jsonify({"message": "Credenciales incorrectas"}), 401
+
+    token = jwt.encode({
+        'user_id': user.id,
+        'exp': datetime.datetime.utcnow() + datetime.timedelta(hours=1)
+    }, 'fabian_es_gay', algorithm='HS256')
+
+    return jsonify({
+        'access_token': token,
+        'message': 'Inicio de sesión exitoso'
+    }), 200
