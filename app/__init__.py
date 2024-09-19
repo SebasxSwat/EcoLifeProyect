20790f7fdbc54cab3268da7941f03a7382ddd628
fabiiann1 <fabianmarinn1@gmail.com>
2024-09-19 07:21:02 -0500

    F

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,27 @@
 from flask import Flask 
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from flask_mail import Mail
 
 db = SQLAlchemy()
+mail = Mail()
 
 def create_app():
     app = Flask(__name__)
     
     app.config.from_object('config.Config')
 
-    db.init_app(app)
+    # Configuración de Flask-Mail
+    app.config['MAIL_SERVER'] = 'smtp.gmail.com'  # Usa el servidor SMTP de tu proveedor de correo
+    app.config['MAIL_PORT'] = 587
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USERNAME'] = app.config.get('MAIL_USERNAME')
+    app.config['MAIL_PASSWORD'] = app.config.get('MAIL_PASSWORD')
+    app.config['MAIL_DEFAULT_SENDER'] = app.config.get('MAIL_DEFAULT_SENDER')
 
-    
+    db.init_app(app)
+    mail.init_app(app)
+
     CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
     from app.routes import authRoute, userRoutes
--- config.py
+++ config.py
@@ -1,4 +1,17 @@
+import os
 
 class Config:
+   
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/ecolife'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+ 
+    MAIL_SERVER = 'smtp.gmail.com'  # Cambia esto si usas otro proveedor de correo
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or 'tu_correo@gmail.com'
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or 'tu_contraseña_de_correo'
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'tu_correo@gmail.com'
+
+    # Clave secreta para Flask
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'una_clave_secreta_muy_segura'
