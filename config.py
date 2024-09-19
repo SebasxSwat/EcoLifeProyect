477593bb4c3d27825a28d7bc561e0999b935b5de
SebasxSwat <sebastianescalante2006@gmail.com>
2024-09-19 15:03:07 -0500

    S

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 class Config:
    
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/ecolifeproyect'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/ecolife'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
  
