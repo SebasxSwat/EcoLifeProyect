24963510c28e0ec73540dac76c2723b5fc494aae
SebasxSwat <sebastianescalante2006@gmail.com>
2024-09-28 19:01:40 -0500

    S

--- config.py
+++ config.py
@@ -1,25 +1,21 @@
 import os
 
 class Config:
-   
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:1234@localhost:3306/ecolife'
+    
+    USER = os.getenv('MYSQLUSER', 'root')
+    PASSWORD = os.getenv('MYSQL_ROOT_PASSWORD', '1234')
+    HOST = os.getenv('RAILWAY_PRIVATE_DOMAIN', 'localhost')
+    DATABASE = os.getenv('MYSQL_DATABASE', 'ecolife')
+
+    SQLALCHEMY_DATABASE_URI = f'mysql+pymysql://{USER}:{PASSWORD}@{HOST}:3306/{DATABASE}'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = 'resetecolife@gmail.com' 
+    MAIL_USERNAME = 'resetecolife@gmail.com'
     MAIL_PASSWORD = 'akgu mndo peif aguy' 
     MAIL_DEFAULT_SENDER = '1097664747f@gmail.com'
 
-    
     JWT_SECRET_KEY = 'ecolifepassword'
     JWT_ALGORITHM = 'HS256'
-
-
-
-
-
-
-#MAIL_USERNAME = '1097664747f@gmail.com'  
-#MAIL_PASSWORD = 'bgxn kibx bujc sjaw'  
